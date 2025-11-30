f9a0bc207cb37a03b214eac9f3b82672b39f9e77
saitmilk <33536254+saitmilk@users.noreply.github.com>
2025-11-30 22:07:51 +0800

    feat: display a circle

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,49 @@
 #include <SFML/Graphics.hpp>
 
+class Game {
+public:
+	Game() : mWindow(sf::VideoMode({ 1920u, 1080u }), "Air Combat"), mPlayer(40.f) {
+		mWindow.setFramerateLimit(144);
+		mPlayer.setPosition({ 100.f, 100.f });
+		mPlayer.setFillColor(sf::Color::Magenta);
+	};
+
+	void run() {
+		while (mWindow.isOpen())
+		{
+			processEvents();
+			update();
+			render();
+		}
+	};
+
+private:
+	void processEvents() {
+		while (const std::optional event = mWindow.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+			{
+				mWindow.close();
+			}
+		}
+	};
+
+	void update() {
+	};
+
+	void render() {
+		mWindow.clear();
+		mWindow.draw(mPlayer);
+		mWindow.display();
+	};
+
+private:
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+};
+
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({ 1920u, 1080u }), "Air Combat");
-    window.setFramerateLimit(144);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
-    }
+	Game game;
+	game.run();
 }
